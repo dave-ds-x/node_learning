a7696d97c30b09312057a7d11bfa99fe9aea3f4c
Davide Di Simone <davide.disimone91@gmail.com>
2024-01-24 18:46:21 +0100

    paths fixed

--- expressJs_views/app.js
+++ expressJs_views/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-
+const path = require("path")
 const bodyParser = require('body-parser')
 
 const app = express()
@@ -17,7 +17,7 @@ app.use("/shop", shopRoutes)
 
 // status wants an http code and in case it verifies, sends what in send function
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found</h1>")
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
 })
 
 
--- expressJs_views/routes/admin.js
+++ expressJs_views/routes/admin.js
@@ -3,14 +3,14 @@ const path = require("path")
 const router = express.Router()
 
 // admin/add-product
-// __dirname is a global variable provided by Node to get the absolute path of this project root
+// __dirname is a global variable provided by Node to get the absolute path of this file
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, "expressJs_views", "views", "admin.html"))
+    res.sendFile(path.join(__dirname, "../", "views", "add_product.html"))
 })
 
 // admin/product
 router.post('/product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, "expressJs_views", "views", "admin.html"))
+    res.sendFile(path.join(__dirname, "../", "views", "shop.html"))
 })
 
 module.exports = router
